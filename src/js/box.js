581315140fd386cc58ae9661048b1228ef098992
Zack Cheng <zack_cheng@brown.edu>
2021-12-08 18:20:28 -0500

    fixes to rise-and-fall blob

--- src/js/box.js
+++ src/js/box.js
@@ -5,6 +5,9 @@ import { camOffsetX, camOffsetZ } from '/js/config'
 let boundX = 6
 let boundZ = 6
 const buildings = new Map()
+const DEBOUNCE_POSITION_THRESHOLD = 0.01
+const BLOB_RADIUS = 4
+const BLOB_RADIUS_SQUARED = BLOB_RADIUS * BLOB_RADIUS
 
 const TILE_WIDTH_HALF = 0.5
 const TILE_HEIGHT_HALF = 0.5
@@ -12,7 +15,10 @@ const TILE_HEIGHT_HALF = 0.5
 function withinBounds(cameraX, cameraZ, boxX, boxZ) {
   const relativeX = boxX - cameraX + camOffsetX
   const relativeZ = boxZ - cameraZ + camOffsetZ
-  return (5 - relativeX * relativeX - relativeZ * relativeZ) / 4
+  const distanceSquared = relativeX * relativeX + relativeZ * relativeZ
+  const result =
+    THREE.MathUtils.smoothstep(BLOB_RADIUS_SQUARED - distanceSquared, 0, BLOB_RADIUS_SQUARED) - 0.05
+  return result
 }
 
 function generateBox(scene, h, x, z, pc, sc) {
@@ -45,7 +51,6 @@ function generateBoxes(scene, centerX, centerZ, cameraX, cameraZ) {
     for (let boxZ = centerZ - boundZ; boxZ <= centerZ + boundZ; boxZ++) {
       const scale = withinBounds(cameraX, cameraZ, boxX, boxZ)
       if (scale > 0) {
-        console.log(scale)
         const h = getNoise(boxX, boxZ, HEIGHT_SEED)
         const pc = getPrimaryColor(boxX, boxZ, 1, 0.5)
         const sc = getSecondaryColor(boxX, boxZ, 1, 0.5)
@@ -68,7 +73,10 @@ export function updateBoxes(scene, camera) {
   // Check if we need to recalculate
   const roundedCameraX = Math.round(cameraX)
   const roundedCameraZ = Math.round(cameraZ)
-  if (Math.abs(cameraX - prevCameraX) < 0.2 && Math.abs(cameraZ - prevCameraZ) < 0.2) {
+  if (
+    Math.abs(cameraX - prevCameraX) < DEBOUNCE_POSITION_THRESHOLD &&
+    Math.abs(cameraZ - prevCameraZ) < DEBOUNCE_POSITION_THRESHOLD
+  ) {
     return
   }
   prevCameraX = cameraX
