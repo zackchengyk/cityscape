bfde18aa1f31c837a10bfd7c8942f89b7104e35a
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2021-12-14 15:34:03 -0500

    gui: increase font size

--- src/js/gui.js
+++ src/js/gui.js
@@ -10,6 +10,7 @@ import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js'
 export function setupGUI(cityscape) {
   cityscape.params = { shadows: true, timeOfDay: 12, rain: false }
   const gui = new GUI()
+  gui.domElement.style = "font-size: 1em"
   gui.add(cityscape.params, 'timeOfDay', 0, 24)
   gui.add(cityscape.params, 'shadows')
   gui.add(cityscape.params, 'rain')
